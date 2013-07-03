4c50732360ef4ec5284de2f2e731cedac7eb5373
Nick Nikolov <nrnickolov@yahoo.com>
2013-07-03 13:20:33 -0400

    Extract locale from accept language header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,18 +3,24 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
    
   def set_locale
-    #I18n.locale = extract_locale_from_tld || I18n.default_locale
-    I18n.locale = 'bg'
+    I18n.locale = extract_locale_from_accept_language_header || I18n.default_locale
+    logger.debug "* Locale set to '#{I18n.locale}'"
   end
 
-  # Get locale from top-level domain or return nil if such locale is not available
-  # You have to put something like:
-  #   127.0.0.1 application.com
-  #   127.0.0.1 application.it
-  #   127.0.0.1 application.pl
-  # in your /etc/hosts file to try this out locally
-  def extract_locale_from_tld
-    parsed_locale = request.host.split('.').last
-    I18n.available_locales.include?(parsed_locale.to_sym) ? parsed_locale  : nil
+  private 
+
+  def extract_locale_from_accept_language_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
   end
+
+  ## Get locale from top-level domain or return nil if such locale is not available
+  ## You have to put something like:
+  ##   127.0.0.1 application.com
+  ##   127.0.0.1 application.it
+  ##   127.0.0.1 application.pl
+  ## in your /etc/hosts file to try this out locally
+  #def extract_locale_from_tld
+  #  parsed_locale = request.host.split('.').last
+  #  I18n.available_locales.include?(parsed_locale.to_sym) ? parsed_locale  : nil
+  #end
 end
